ffd40c6ff21d680581653c6b450c9cf74400a2b2
nicolas peraudeau <peraudeau.nicolas2@gmail.com>
2021-11-08 13:14:52 +0100

    correction erreur

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,18 +1,23 @@
 let url = "http://localhost:3000/api/teddies";
 
-fetch(url).then((reponse) =>
-  reponse.json().then((data) => {
-    let affichage = '<section id="produits">';
-    for (let ours of data) {
-      affichage += `<a href="produit.html?id=${ours._id}">`;
-      affichage += "<article>";
-      affichage += `<img src="${ours.imageUrl}" alt="photo ours en peluche" class="image_article"/>`;
-      affichage += `<p class="nom">${ours.name}</p>`;
-      affichage += `<p>${ours.price / 100}€</p>`;
-      affichage += "</article>";
-      affichage += "</a>";
-    }
-    affichage += "</section>";
+fetch(url)
+  .then((reponse) =>
+    reponse.json().then((data) => {
+      let affichage = '<section id="produits">';
+      for (let ours of data) {
+        affichage += `<a href="produit.html?id=${ours._id}">`;
+        affichage += "<article>";
+        affichage += `<img src="${ours.imageUrl}" alt="photo ours en peluche" class="image_article"/>`;
+        affichage += `<p class="nom">${ours.name}</p>`;
+        affichage += `<p>${ours.price / 100}€</p>`;
+        affichage += "</article>";
+        affichage += "</a>";
+      }
+      affichage += "</section>";
+      document.querySelector("#main").innerHTML = affichage;
+    })
+  )
+  .catch(function () {
+    let affichage = `<p>Les articles n'ont pas pu ce charger verifier que le serveur local est bien lancé</p>`;
     document.querySelector("#main").innerHTML = affichage;
-  })
-);
+  });
--- frontend/js/produit.js
+++ frontend/js/produit.js
@@ -2,46 +2,50 @@ let params = new URL(document.location).searchParams;
 let id = params.get("id");
 
 let url = "http://localhost:3000/api/teddies/" + id;
-fetch(url).then((reponse) =>
-  reponse.json().then((data) => {
-    let imageSecondaire = document.querySelector("#images_secondaire");
-    imageSecondaire.innerHTML = `<img src="${data.imageUrl}" alt="peluche photo 1"/><img src="${data.imageUrl}" alt="peluche photo 2"/><img src="${data.imageUrl}" alt="peluche photo 3"/>`;
-    let imagePrincipale = document.querySelector("#image_principale");
-    imagePrincipale.setAttribute("src", `${data.imageUrl}`);
-    imagePrincipale.setAttribute("alt", "photo principal peluche");
-    document.querySelector("h2").textContent = data.name;
-    let price = data.price / 100;
-    document.querySelector("#price").textContent = `${price}€`;
-    let option = '<option value="">Couleurs</option>';
-    for (let couleur of data.colors) {
-      option += `<option value="${couleur}">${couleur}</option>`;
-    }
-    document.querySelector("#couleur-select").innerHTML = option;
-    document.querySelector("#description").textContent = data.description;
-
-    document.querySelector(".bouton").addEventListener("click", function () {
-      let commande = {
-        image: data.imageUrl,
-        prix: data.price / 100,
-        nom: data.name,
-        id: data._id,
-      };
-      let produitLocalStorage = JSON.parse(localStorage.getItem("produit"));
-
-      if (produitLocalStorage) {
-        produitLocalStorage.push(commande);
-        localStorage.setItem("produit", JSON.stringify(produitLocalStorage));
-      } else {
-        produitLocalStorage = [];
-        produitLocalStorage.push(commande);
-        localStorage.setItem("produit", JSON.stringify(produitLocalStorage));
+fetch(url)
+  .then((reponse) =>
+    reponse.json().then((data) => {
+      let imageSecondaire = document.querySelector("#images_secondaire");
+      imageSecondaire.innerHTML = `<img src="${data.imageUrl}" alt="peluche photo 1"/><img src="${data.imageUrl}" alt="peluche photo 2"/><img src="${data.imageUrl}" alt="peluche photo 3"/>`;
+      let imagePrincipale = document.querySelector("#image_principale");
+      imagePrincipale.setAttribute("src", `${data.imageUrl}`);
+      imagePrincipale.setAttribute("alt", "photo principal peluche");
+      document.querySelector("h2").textContent = data.name;
+      let price = data.price / 100;
+      document.querySelector("#price").textContent = `${price}€`;
+      let option = '<option value="">Couleurs</option>';
+      for (let couleur of data.colors) {
+        option += `<option value="${couleur}">${couleur}</option>`;
       }
+      document.querySelector("#couleur-select").innerHTML = option;
+      document.querySelector("#description").textContent = data.description;
 
-      document.querySelector(".bouton p").textContent =
-        "Article ajouté au panier ✔";
-      setTimeout(function () {
-        document.querySelector(".bouton p").textContent = "Ajouter au panier";
-      }, 1500);
-    });
-  })
-);
+      document.querySelector(".bouton").addEventListener("click", function () {
+        let commande = {
+          image: data.imageUrl,
+          prix: data.price / 100,
+          nom: data.name,
+          id: data._id,
+        };
+        let produitLocalStorage = JSON.parse(localStorage.getItem("produit"));
+
+        if (produitLocalStorage) {
+          produitLocalStorage.push(commande);
+          localStorage.setItem("produit", JSON.stringify(produitLocalStorage));
+        } else {
+          produitLocalStorage = [];
+          produitLocalStorage.push(commande);
+          localStorage.setItem("produit", JSON.stringify(produitLocalStorage));
+        }
+
+        document.querySelector(".bouton p").textContent =
+          "Article ajouté au panier ✔";
+        setTimeout(function () {
+          document.querySelector(".bouton p").textContent = "Ajouter au panier";
+        }, 1500);
+      });
+    })
+  )
+  .catch(function () {
+    window.location.href = "index.html";
+  });
